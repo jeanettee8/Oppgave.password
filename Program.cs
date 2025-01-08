2e024c547fa2134520568bc25be814757acb79d7
jeanettee8 <ekje112024@gmail.com>
2025-01-08 11:14:04 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string password="12345678";
+
+if (password.Length<8) {
+    Console.WriteLine("Weak password, you neeed to have 8 or more characters in your password");
+}
+
+else if (password=="12345678") {
+    Console.WriteLine("Your password can't be \"12345678\"");
+}
+
+else if (password=="password") {
+    Console.WriteLine("Your password can't be \"password\"");
+}
+
+else {
+    Console.WriteLine("Strong password");
+}
